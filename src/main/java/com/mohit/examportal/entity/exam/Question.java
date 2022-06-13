69711ad79b16606f709a3d31dc59f36bd8467ac2
mohittiwari5 <mohittiwarimt127@gmail.com>
2022-06-13 18:24:00 +0530

    Resolved issue in Question class of @JsonIgnore where saving data also ignores the answer

--- src/main/java/com/mohit/examportal/entity/exam/Question.java
+++ src/main/java/com/mohit/examportal/entity/exam/Question.java
@@ -1,6 +1,7 @@
 package com.mohit.examportal.entity.exam;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,8 +9,6 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "question")
-@Getter
-@Setter
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -21,7 +20,6 @@ public class Question {
     private String option3;
     private String option4;
 
-    @JsonIgnore
     private String answer;
 
     @Transient
@@ -29,6 +27,90 @@ public class Question {
 
     @ManyToOne
     private Quiz quiz;
+
+    public Long getQuesId() {
+        return quesId;
+    }
+
+    public void setQuesId(Long quesId) {
+        this.quesId = quesId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getOption1() {
+        return option1;
+    }
+
+    public void setOption1(String option1) {
+        this.option1 = option1;
+    }
+
+    public String getOption2() {
+        return option2;
+    }
+
+    public void setOption2(String option2) {
+        this.option2 = option2;
+    }
+
+    public String getOption3() {
+        return option3;
+    }
+
+    public void setOption3(String option3) {
+        this.option3 = option3;
+    }
+
+    public String getOption4() {
+        return option4;
+    }
+
+    public void setOption4(String option4) {
+        this.option4 = option4;
+    }
+
+    @JsonIgnore
+    public String getAnswer() {
+        return answer;
+    }
+
+    @JsonProperty("answer")
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getGivenAnswer() {
+        return givenAnswer;
+    }
+
+    public void setGivenAnswer(String givenAnswer) {
+        this.givenAnswer = givenAnswer;
+    }
+
+    public Quiz getQuiz() {
+        return quiz;
+    }
+
+    public void setQuiz(Quiz quiz) {
+        this.quiz = quiz;
+    }
+
+
 
 
 }
